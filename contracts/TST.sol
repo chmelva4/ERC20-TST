e651c5f84a50cad8d8a2d0f1e74b3dfd2340c4ff
Václav Chmel <vasekchmel@gmail.com>
2020-07-09 11:23:46 +0200

    updated token to deploy

--- contracts/TST.sol
+++ contracts/TST.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.4.0 <0.7.0;
 import './StandardToken.sol';
 
 contract TST is StandardToken {
-    string public name = 'Test Standard Token';
-    string public symbol = 'TST';
-    uint public decimals = 18;
+    string public name = 'CHM4 Test Token';
+    string public symbol = 'CHM4';
+    uint public decimals = 0;
 
     function showMeTheMoney(address _to, uint256 _value) public {
         totalSupply += _value;
