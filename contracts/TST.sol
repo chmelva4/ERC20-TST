393462736c7a220bbebc7e8c5a3fe56bfd305626
Václav Chmel <vasekchmel@gmail.com>
2020-07-09 11:19:07 +0200

    fixed compilation errors

--- contracts/TST.sol
+++ contracts/TST.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.4.0;
+pragma solidity >=0.4.0 <0.7.0;
 
-import 'StandardToken.sol';
+import './StandardToken.sol';
 
 contract TST is StandardToken {
     string public name = 'Test Standard Token';
     string public symbol = 'TST';
     uint public decimals = 18;
 
-    function showMeTheMoney(address _to, uint256 _value) {
+    function showMeTheMoney(address _to, uint256 _value) public {
         totalSupply += _value;
         balances[_to] += _value;
-        Transfer(0, _to, _value);
+        emit Transfer(address(0), _to, _value);
     }
 }
